c942024f369207cb286ea6d47f55b97e63763919
Higor <omegadosgames02@gmail.com>
2020-10-20 17:52:10 -0300

    botoes e inicio função adicionar musica

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from tkinter import *
+import pygame
+from tkinter import filedialog
 
 
+pygame.init()
 root = Tk()
 
 
@@ -10,12 +13,14 @@ class Player:
         self.root = root
         self.window()
         self.labels()
+        self.buttons()
         root.mainloop()
 
     def window(self, **kwargs):
         self.w_height = 440
         self.w_width = 630
         self.root.title('MP3 PLAYER')
+        self.root.configure(background='#dde')
 
         try:
             self.w_width = kwargs['width']
@@ -36,8 +41,24 @@ class Player:
                                             self.w_height,
                                             self.left, self.top))
     def labels(self):
-        self.winmus = Text(self.root, bg='white')
+        self.winmus = Entry(self.root, bg='white')
         self.winmus.configure(state='disabled')
         self.winmus.place(height=300, width=300, x=165, bordermode=INSIDE)
+    
+    def buttons(self):
+        self.previous = Button(self.root, text='<')
+        self.previous.place(x=165, y=300, width=30)
+
+        self.play = Button(self.root, text='Play/Pause')
+        self.play.place(x=195, y=300, width=100)
+
+        self.next = Button(self.root, text='>')
+        self.next.place(x=295, y=300, width=30)
+
+        self.add_ = Button(self.root, text='Adicionar música', command=self.add_msc)
+        self.add_.place(x=325, y=300)
+
+    def add_msc(self):
+        self.dir = filedialog.askopenfilenames()
 
 Player(root)
